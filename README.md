913cb105a6f9f7e597b5b09eba82c0836b38e8d2
N7-Rvenant <dmitri.zol@rambler.ru>
2021-12-13 12:18:05 +0300

    phonecall_checker: AMI connection creation added

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Asterisk interaction prototype - Тестовое окружение для развертывания Asterisk и набор прототипов, предоставляющих пример организации работы с AMI из Python
 
- * panoramisk/phonecall_checker - пример инициации звонка в Asterisk с его последующей периодической проверкой
+ * panoramisk/phonecall_checker - пример инициации звонка в Asterisk с его последующей периодической проверкой его существования
--- panoramisk/phonecall_checker/main.py
+++ panoramisk/phonecall_checker/main.py
@@ -1,7 +1,28 @@
 import asyncio
+import panoramisk
+
+
+AMI_CONNECTION_CONFIG = {
+    'host': '192.168.50.41',
+    'port': 5038,
+    'username': 'username',
+    'secret': 'secret'
+}
+AMI_CONNECTION_TIMEOUT = 3
 
 
 async def work():
+    ami_manager = panoramisk.Manager(**AMI_CONNECTION_CONFIG)
+
+    print("Attempting AMI connection...")
+    await asyncio.wait((ami_manager.connect(), ), return_when=asyncio.FIRST_COMPLETED)
+    await asyncio.sleep(AMI_CONNECTION_TIMEOUT)
+    if not ami_manager._connected:
+        print("AMI connection attempt failed!")
+        return
+    else:
+        print("AMI connection has been established!")
+
     interval = 5
     while True:
         print("Worker waits for %s seconds..." % interval)
@@ -14,7 +35,7 @@ def main():
     loop = asyncio.get_event_loop()
     task = loop.create_task(work())
     try:
-        loop.run_forever()
+        loop.run_until_complete(task)
     except KeyboardInterrupt:
         task.cancel()
     loop.stop()
